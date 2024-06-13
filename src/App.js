3e0a612f3dcf2723f7e25d025fd919e83c16d66e
APCP12 <pereiraafonso464@gmail.com>
2024-06-13 10:05:10 +0100

    hjvb

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 
 function App() {
+  const name = '.';
   return (
     <div className="App">
       <h1>Hello World</h1>
-    
+      <p>Hello {name}!</p>
     </div>
   );
 }
